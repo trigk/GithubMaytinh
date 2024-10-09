1002b4c4042d13418baa6a7fb292e1217376dab7
ou mai goss <haison27102003@gmail.com>
2024-10-09 23:45:15 +0700

    Chuc nang cong

--- Maytinh/Form1.cs
+++ Maytinh/Form1.cs
@@ -29,7 +29,16 @@ namespace Maytinh
 
         private void btnnhan_Click(object sender, EventArgs e)
         {
-
+            double number1, number2;
+            if (double.TryParse(txtso1.Text, out number1) && double.TryParse(txtso2.Text, out number2))
+            {
+                double result = number1 * number2;
+                txtKQ.Text = result.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập số hợp lệ!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void btnchia_Click(object sender, EventArgs e)
